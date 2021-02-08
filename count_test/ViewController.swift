9f0e5bc5f0c873546f243bd26fa6372883a32973
ayu129 <ayu.daddybear.29@gmail.com>
2021-02-08 14:45:40 +0900

    countapp_test

--- count_test/ViewController.swift
+++ count_test/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count: Int=0
+    @IBOutlet var countLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func plus(){
+        count = count + 1
+        countLabel.text = String(count)
+    }
+    
+    @IBAction func minus(){
+        count = count - 1
+        countLabel.text = String(count)
+    }
+    
+    @IBAction func clear(){
+        count = 0
+        countLabel.text = String(count)
     }
 
 
