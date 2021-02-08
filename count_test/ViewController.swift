b9f767157e86b3151f3e2c81ac38da5297ec8718
ayu129 <ayu.daddybear.29@gmail.com>
2021-02-08 13:34:26 +0900

    Initial Commit

---
+++ count_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  count_test
+//
+//  Created by 伊藤愛結 on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
